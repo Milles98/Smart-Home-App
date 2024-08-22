76bfa7b3c52948a560bac02987294d3dd8eed3c3
Mille Elfver <mille.elfver98@gmail.com>
2024-08-22 13:58:41 +0200

    Add new properties to Card class and fix property names
    - Updated `using` directives to include `System.Windows` and `System.Windows.Media`.
    - Added new dependency properties to `Card` class: `Title` (string), `IsChecked` (bool), `IsHorizontal` (bool), `ImageOn` (ImageSource), and `ImageOff` (ImageSource).
    - Corrected `DependencyProperty.Register` calls for `IsHorizontalProperty`, `ImageOnProperty`, and `ImageOffProperty` to use the correct property names instead of "Title".

--- Smart Home App/UserControls/Card.xaml.cs
+++ Smart Home App/UserControls/Card.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Smart_Home_App.UserControls;
 
@@ -8,4 +10,46 @@ public partial class Card : UserControl
 	{
 		InitializeComponent();
 	}
+
+	public string Title
+	{
+		get { return (string)GetValue(TitleProperty); }
+		set { SetValue(TitleProperty, value); }
+	}
+	public static readonly DependencyProperty TitleProperty =
+		DependencyProperty.Register("Title", typeof(string), typeof(Card));
+
+	public bool IsChecked
+	{
+		get { return (bool)GetValue(IsCheckedProperty); }
+		set { SetValue(IsCheckedProperty, value); }
+	}
+	public static readonly DependencyProperty IsCheckedProperty =
+		DependencyProperty.Register("IsChecked", typeof(bool), typeof(Card));
+
+	public bool IsHorizontal
+	{
+		get { return (bool)GetValue(IsHorizontalProperty); }
+		set { SetValue(IsHorizontalProperty, value); }
+	}
+	public static readonly DependencyProperty IsHorizontalProperty =
+		DependencyProperty.Register("Title", typeof(bool), typeof(Card));
+
+	public ImageSource ImageOn
+	{
+		get { return (ImageSource)GetValue(ImageOnProperty); }
+		set { SetValue(ImageOnProperty, value); }
+	}
+	public static readonly DependencyProperty ImageOnProperty =
+		DependencyProperty.Register("Title", typeof(ImageSource), typeof(Card));
+
+	public ImageSource ImageOff
+	{
+		get { return (ImageSource)GetValue(ImageOffProperty); }
+		set { SetValue(ImageOffProperty, value); }
+	}
+	public static readonly DependencyProperty ImageOffProperty =
+		DependencyProperty.Register("Title", typeof(ImageSource), typeof(Card));
+
+
 }

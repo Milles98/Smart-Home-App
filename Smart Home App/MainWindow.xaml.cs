726c0e9a2f28a31bae7aabb5aec8792570bdfa93
Mille Elfver <mille.elfver98@gmail.com>
2024-08-22 15:27:48 +0200

    Update UI animations and window state handling
    Modified MainWindow.xaml to change page2 opacity animation.
    Cleaned up using directives and reformatted namespace in MainWindow.xaml.cs.
    Updated event handlers to improve window state management.
    Added IsMaximized field to track window state.

--- Smart Home App/MainWindow.xaml.cs
+++ Smart Home App/MainWindow.xaml.cs
@@ -1,34 +1,42 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Smart_Home_App
+namespace Smart_Home_App;
+
+public partial class MainWindow : Window
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
+	public MainWindow()
 	{
-		public MainWindow()
+		InitializeComponent();
+	}
+
+	private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+	{
+		if (e.ChangedButton == MouseButton.Left)
 		{
-			InitializeComponent();
+			this.DragMove();
 		}
+	}
 
-		private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+	bool IsMaximized = false;
+	private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+	{
+		if (e.ClickCount == 2)
 		{
-
-		}
-
-		private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-		{
-
+			if (IsMaximized)
+			{
+				this.WindowState = WindowState.Normal;
+				this.Width = 1080;
+				this.Height = 720;
+				IsMaximized = false;
+			}
+			else
+			{
+				this.WindowState = WindowState.Maximized;
+				this.Width = SystemParameters.PrimaryScreenWidth;
+				this.Height = SystemParameters.PrimaryScreenHeight;
+				IsMaximized = true;
+			}
 		}
 	}
 }
